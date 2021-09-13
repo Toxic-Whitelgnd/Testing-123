d44b218e921d1c358b9bd30c1cbf52c4928501b2
BigBoyNoob <88152172+BigBoyNoob@users.noreply.github.com>
2021-09-13 16:32:47 +0530

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+struct student_data{
+
+    char name;
+    float marks;
+    char Class;
+};
+
+void main(){
+int i;
+    while(i<6)
+    {printf("Enter the student's name:");
+    scanf("%s",&student_data.s[i].name);}
+
+
+    for(i=0;i<6;i++)
+    {
+        printf("The name of the students:", student_data.s[i].name);
+
+    }
+
+}
+
